44507a5047f46747e2c5ab9e7224e87d1c834104
Alexander Obozny <alexander.obozny@gmail.com>
2017-10-08 23:26:27 +0300

    Maze generation algorithm has been implemented

--- src/main/java/ao/maze/gui/Maze.java
+++ src/main/java/ao/maze/gui/Maze.java
@@ -1,6 +1,6 @@
 package ao.maze.gui;
 
-import java.util.Random;
+import java.util.*;
 
 public class Maze {
 
@@ -15,19 +15,52 @@ public class Maze {
             for (int x = 0; x < mazeWidth; x++) {
                 int cellX = gameFieldX + MazeCell.CELL_SIDE_LENGTH * x;
                 int cellY = gameFieldY + MazeCell.CELL_SIDE_LENGTH * y;
-                maze[y][x] = new MazeCell(cellX, cellY);
+                maze[y][x] = new MazeCell(cellX, cellY, x, y);
             }
         }
 
         this.player = player;
-        // TODO temporary solution for debug, should be actual maze enter. Change it after maze generation
+
+        // Maze generation algorithm has been took from here: https://habrahabr.ru/post/262345/
         Random random = new Random();
-        int entryPointRowIndex = random.nextInt(mazeHeight);
         playerCurrentCellX = 0;
-        playerCurrentCellY = entryPointRowIndex;
-        maze[entryPointRowIndex][0].removeLeftSide();
-        maze[entryPointRowIndex][0].removeRightSide();
-        maze[entryPointRowIndex][0].setPlayer(this.player);
+        playerCurrentCellY = random.nextInt(mazeHeight);
+        maze[playerCurrentCellY][playerCurrentCellX].removeLeftSide();
+        maze[playerCurrentCellY][playerCurrentCellX].setPlayer(this.player);
+        maze[playerCurrentCellY][playerCurrentCellX].visit();
+        MazeCell currentCell = maze[playerCurrentCellY][playerCurrentCellX];
+
+        Deque<MazeCell> stack = new ArrayDeque<>();
+        while (isUnvisitedCellsExist()) {
+            List<MazeCell> unvisitedNeighbors = getUnvisitedNeighbors(maze, currentCell);
+            if (!unvisitedNeighbors.isEmpty()) {
+                stack.push(currentCell);
+                MazeCell randomUnvisitedNeighborCell = unvisitedNeighbors.get(random.nextInt(unvisitedNeighbors.size()));
+                if (currentCell.getX() == randomUnvisitedNeighborCell.getX() + 1) {
+                    currentCell.removeLeftSide();
+                    randomUnvisitedNeighborCell.removeRightSide();
+                } else if (currentCell.getX() == randomUnvisitedNeighborCell.getX() - 1) {
+                    currentCell.removeRightSide();
+                    randomUnvisitedNeighborCell.removeLeftSide();
+                } else if (currentCell.getY() == randomUnvisitedNeighborCell.getY() + 1) {
+                    currentCell.removeTopSide();
+                    randomUnvisitedNeighborCell.removeBottomSide();
+                } else {
+                    currentCell.removeBottomSide();
+                    randomUnvisitedNeighborCell.removeTopSide();
+                }
+                currentCell = randomUnvisitedNeighborCell;
+                currentCell.visit();
+            } else if (!stack.isEmpty()) {
+                currentCell = stack.pop();
+            } else {
+                // with proper algorithm implementation and proper input data this case should never be executed
+                throw new RuntimeException("Something went wrong. Please, check algorithm and input data");
+            }
+        }
+        int exitX = mazeWidth - 1;
+        int exitY = random.nextInt(mazeHeight);
+        maze[exitY][exitX].removeRightSide();
     }
 
     MazeCell[][] getMatrix() {
@@ -48,6 +81,59 @@ public class Maze {
 
     void setPlayerCurrentCellY(int playerCurrentCellY) {
         this.playerCurrentCellY = playerCurrentCellY;
+    }
+
+    private boolean isUnvisitedCellsExist() {
+        for (MazeCell[] row : maze) {
+            for (MazeCell cell : row) {
+                if (!cell.isVisited()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static List<MazeCell> getUnvisitedNeighbors(MazeCell[][] maze, MazeCell cell) {
+        List<MazeCell> unvisitedNeighbors = new ArrayList<>();
+        int xIndex = cell.getX();
+        int yIndex = cell.getY();
+
+        if (xIndex != 0 && xIndex != maze[yIndex].length - 1) {
+            if (!maze[yIndex][xIndex - 1].isVisited()) {
+                unvisitedNeighbors.add(maze[yIndex][xIndex - 1]);
+            }
+            if (!maze[yIndex][xIndex + 1].isVisited()) {
+                unvisitedNeighbors.add(maze[yIndex][xIndex + 1]);
+            }
+        } else if (xIndex == 0) {
+            if (!maze[yIndex][xIndex + 1].isVisited()) {
+                unvisitedNeighbors.add(maze[yIndex][xIndex + 1]);
+            }
+        } else {
+            if (!maze[yIndex][xIndex - 1].isVisited()) {
+                unvisitedNeighbors.add(maze[yIndex][xIndex - 1]);
+            }
+        }
+
+        if (yIndex != 0 && yIndex != maze.length - 1) {
+            if (!maze[yIndex - 1][xIndex].isVisited()) {
+                unvisitedNeighbors.add(maze[yIndex - 1][xIndex]);
+            }
+            if (!maze[yIndex + 1][xIndex].isVisited()) {
+                unvisitedNeighbors.add(maze[yIndex + 1][xIndex]);
+            }
+        } else if (yIndex == 0) {
+            if (!maze[yIndex + 1][xIndex].isVisited()) {
+                unvisitedNeighbors.add(maze[yIndex + 1][xIndex]);
+            }
+        } else {
+            if (!maze[yIndex - 1][xIndex].isVisited()) {
+                unvisitedNeighbors.add(maze[yIndex - 1][xIndex]);
+            }
+        }
+
+        return unvisitedNeighbors;
     }
 
 }
--- src/main/java/ao/maze/gui/MazeCell.java
+++ src/main/java/ao/maze/gui/MazeCell.java
@@ -7,17 +7,42 @@ public class MazeCell {
 
     static final int CELL_SIDE_LENGTH = 30;
 
+    private final int x;
+    private final int y;
+    private boolean visited;
     private Shape leftSide;
     private Shape topSide;
     private Shape rightSide;
     private Shape bottomSide;
     private Player player;
 
-    MazeCell(int x, int y) {
-        leftSide = new Line2D.Double(x, y, x, y + CELL_SIDE_LENGTH);
-        topSide = new Line2D.Double(x, y, x + CELL_SIDE_LENGTH, y);
-        rightSide = new Line2D.Double(x + CELL_SIDE_LENGTH, y, x + CELL_SIDE_LENGTH, y + CELL_SIDE_LENGTH);
-        bottomSide = new Line2D.Double(x, y + CELL_SIDE_LENGTH, x + CELL_SIDE_LENGTH, y + CELL_SIDE_LENGTH);
+    MazeCell(int topLeftXCoordinate, int topLeftYCoordinate, int x, int y) {
+        leftSide = new Line2D.Double(topLeftXCoordinate, topLeftYCoordinate,
+                topLeftXCoordinate, topLeftYCoordinate + CELL_SIDE_LENGTH);
+        topSide = new Line2D.Double(topLeftXCoordinate, topLeftYCoordinate,
+                topLeftXCoordinate + CELL_SIDE_LENGTH, topLeftYCoordinate);
+        rightSide = new Line2D.Double(topLeftXCoordinate + CELL_SIDE_LENGTH, topLeftYCoordinate,
+                topLeftXCoordinate + CELL_SIDE_LENGTH, topLeftYCoordinate + CELL_SIDE_LENGTH);
+        bottomSide = new Line2D.Double(topLeftXCoordinate, topLeftYCoordinate + CELL_SIDE_LENGTH,
+                topLeftXCoordinate + CELL_SIDE_LENGTH, topLeftYCoordinate + CELL_SIDE_LENGTH);
+        this.x = x;
+        this.y = y;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    boolean isVisited() {
+        return visited;
+    }
+
+    void visit() {
+        visited = true;
     }
 
     public Shape getLeftSide() {
